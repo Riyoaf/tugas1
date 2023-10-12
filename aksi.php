9ff74da3b54f3a20631becbd528ddba2b00772fb
Riyoaf <146907179+Riyoaf@users.noreply.github.com>
2023-10-12 21:49:16 +0800

    Update aksi.php

--- aksi.php
+++ aksi.php
@@ -7,31 +7,26 @@
     <style>
         body {
             font-family: Arial, sans-serif;
-            background-color: #f0f0f0; 
+            background-color: #f5f5f5;
             text-align: center;
         }
 
         .container {
-            background-color: #ffffff; 
+            background-color: #fff;
             border-radius: 8px;
-            box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.2);
+            box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.1);
             max-width: 400px;
             margin: 20px auto;
             padding: 20px;
         }
 
         h1 {
-            color: #007BFF; 
+            color: #007BFF;
         }
 
         p {
             text-align: left;
-            margin: 10px 0; 
-            color: #333; 
-        }
-
-        strong {
-            color: #007BFF; 
+            margin: 5px 0;
         }
     </style>
 </head>
